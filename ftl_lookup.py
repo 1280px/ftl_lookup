c5da7b1085487c7552220129577629a21516691a
twelve eighty pix. <71165491+1280px@users.noreply.github.com>
2025-09-05 22:48:42 +0700

    Cleaner code and comment in English, fix repo lists desync
    hi there! :D

--- ftl_lookup.py
+++ ftl_lookup.py
@@ -4,191 +4,181 @@ from datetime import datetime
 import time
 import base64
 
+URL_REPOS_LIST = 'https://api.github.com/search/repositories?q=cosmic%20org%3Apop-os'
+URL_FTL_LOOKUP = 'https://api.github.com/search/code?q=repo%3Apop-os%2F{repo}%20extension%3Aftl'
+URL_FTL_COMMIT = 'https://api.github.com/repos/pop-os/{repo}/commits'
+
 LOCALE_SRC = 'en'
 LOCALE_DST = 'ru'
-RQ_DELAY = 1
+
+REQ_DELAY = 1
 RETRY_DELAY = 5
 MAX_RETRIES = 10
 
+req_cnt = 0
+req_sec = 0
 
-rq_cnt = 0
-time_cnt = 0
 
 def make_request(url, headers, params=None):
-    global rq_cnt, time_cnt
+    global req_cnt, req_sec
 
     for r in range(MAX_RETRIES):
-        time_cnt += RQ_DELAY
-        time.sleep(RQ_DELAY) # Пробуем изначально вести себя хорошо и не превышать лимит запросов
+        req_sec += REQ_DELAY
+        time.sleep(REQ_DELAY) # try to behave nice from the start :)
 
-        print('.', end='' , flush=True)
-        rq_cnt += 1
+        print('.', end='', flush=True)
+        req_cnt += 1
         res = requests.get(url, headers=headers, params=params)
 
-        if res.status_code == 403: # Превышение числа запросов GH шлёт именно под кодом 403
-            time_cnt += RETRY_DELAY
+        if res.status_code == 403: # GH sends too many requests error as 403, for some reason
+            req_sec += RETRY_DELAY
             time.sleep(RETRY_DELAY)
         else:
-            res.raise_for_status() # Код != 200 --> Выводим ошибку
+            res.raise_for_status() # status code != 200 --> show error
             print(' ', end='', flush=True)
             return res
 
-    print('\n=== Превышено макисмальное число попыток получения данных от GH API! ===')
+    print('\nAllowed max number of GitHub API request attemps exceeded!')
     exit(-1)
 
+def get_ftl_text(url):
+    res = make_request(
+        url,
+        headers={
+            'Authorization': f'Bearer {a}'
+        }
+    )
+    ftl_text_raw = res.json()
 
-print(f'=== Состояние компонентов перевода на {str(datetime.now())} ===')
+    ftl_text = base64.b64decode(ftl_text_raw['content']) # GH returns content as Base64
+    ftl_text = ftl_text.decode('UTF-8') # convert from Unicode to UTF-8
+    ftl_text = ftl_text.replace('\n\n', '\n') # remove empty lines
+    if (ftl_text.endswith('\n')):
+        ftl_text = ftl_text[:-1] # remove extra empty line at the end, if present
 
-a = None
-if os.path.isfile('./a.txt'):
-    a = open('./a.txt', 'r').read().split('\n')[0]
-else:
-    print('\nОстутствует файл ключа аутентификации a.txt (ПАПОЧКА ЗОЛ....)')
-    exit(-1)
+    return ftl_text
 
+def get_ftl_date(repo, path):
+    res = make_request(
+        URL_FTL_COMMIT.format(repo=repo),
+        headers={
+            'Authorization': f'Bearer {a}'
+        },
+        params={
+            'path': path, # get just the file we need
+            'per_page': 1 # we only care about the latest commit
+        }
+    )
+    ftl_date_raw = res.json()
 
-res = make_request(
-    'https://api.github.com/search/repositories?q=cosmic%20org%3Apop-os',
-    headers={
-        'Authorization': f'Bearer {a}'
-    },
-    params={
-        'per_page': 100
-    }
-)
+    ftl_date = ftl_date_raw[0]['commit']['committer']['date'] # get commit date
+    ftl_date = datetime.strptime(ftl_date, '%Y-%m-%dT%H:%M:%SZ') # parse it...
+    ftl_date = ftl_date.timestamp() # ...and derive the timestamp!
 
-repos_raw = res.json()
-repos = [repo['name'] for repo in repos_raw['items']]
-print(f'\nНайдено репозиториев: {len(repos)}')
-
-
-for repo in repos:
-    print(f'\n{repo}')
-
-    ftls_page = 1
-    ftls_allitems = []
-    while True:
-        res = make_request(
-            f'https://api.github.com/search/code?q=repo%3Apop-os%2F{repo}%20extension%3Aftl',
-            headers={
-                'Authorization': f'Bearer {a}'
-            },
-            params={
-                'per_page': 30, 'page': ftls_page
-            }
-        )
-
-        ftls_raw = res.json()
-        ftls_allitems.extend(ftls_raw['items'])
-
-        if len(ftls_raw['items']) < 30:
-            break # Достигнут конец пагинации, записаны все ftl-файлы
-        else:
-            ftls_page += 1 # Переходим на след. страницу
-    
-    # Случай 1 -- В компоненте нечего переводить
-    if (len(ftls_allitems) == 0):
-        print('➖')
-        continue
+    return ftl_date
 
 
-    ftls_src_meta = list(filter(lambda m: f'/{LOCALE_SRC}/' in m['path'], ftls_allitems))
-    ftls_dst_meta = list(filter(lambda m: f'/{LOCALE_DST}/' in m['path'], ftls_allitems))
+if __name__ == "__main__":
+    print(f'=== State of components\' translations as of {str(datetime.now()).split('.')[0]} ===')
 
-    ftl_src_names = set([ftls_src_meta[i]['name'] for i in range(len(ftls_src_meta))])
-    ftl_dst_names = set([ftls_dst_meta[i]['name'] for i in range(len(ftls_dst_meta))])
-    flt_names_difference = ftl_src_names - ftl_dst_names
+    a = None
+    if os.path.isfile('./a.txt'):
+        a = open('./a.txt', 'r').read().strip()
+    else:
+        print('\nUnable to open GitHub API кеу file (a.txt) -- ПАПОЧКА ЗОЛ....')
+        exit(-1)
 
-    # Случай 2 -- Требуемая локаль компонента отсутствует полностью
-    for name in flt_names_difference:
-        print(f'❌ {LOCALE_DST}/{name} не существует в https://github.com/pop-os/{repo}')
+
+    res = make_request(
+        URL_REPOS_LIST,
+        headers={
+            'Authorization': f'Bearer {a}'
+        },
+        params={
+            'per_page': 100
+        }
+    )
+
+    repos_raw = res.json()
+    repos = [repo['name'] for repo in repos_raw['items']]
+    print(f'\nFound suitable repos: {len(repos)}')
 
 
-    for ftl_src_meta, ftl_dst_meta in zip(ftls_src_meta, ftls_dst_meta):
-        res = make_request(
-            ftl_src_meta['url'],
-            headers={
-                'Authorization': f'Bearer {a}'
-            }
-        )
+    for repo in repos:
+        print(f'\n{repo}', end='')
 
-        ftl_src_raw = res.json()
-        ftl_src = base64.b64decode(ftl_src_raw['content']) # GH отправляет контент в виде base64
-        ftl_src = ftl_src.decode('UTF-8') # Переводим из Unicode в UTF-8
-        ftl_src = ftl_src.replace('\n\n', '\n') # Удаляем пустые строки
-        if (ftl_src.endswith('\n')):
-            ftl_src = ftl_src[:-1] # Удаляем перенос строки из конца данных, если есть
+        ftls_page = 1 # pagination starts from 1st page, not 0th
+        ftls_allitems = []
 
-        res = make_request(
-            ftl_dst_meta['url'],
-            headers={
-                'Authorization': f'Bearer {a}'
-            }
-        )
+        while True:
+            res = make_request(
+                URL_FTL_LOOKUP.format(repo=repo),
+                headers={
+                    'Authorization': f'Bearer {a}'
+                },
+                params={
+                    'per_page': 30, 'page': ftls_page
+                }
+            )
 
-        ftl_dst_raw = res.json()
-        ftl_dst = base64.b64decode(ftl_dst_raw['content']) # GH отправляет контент в виде base64
-        ftl_dst = ftl_dst.decode('UTF-8') # Переводим из Unicode в UTF-8
-        ftl_dst = ftl_dst.replace('\n\n', '\n') # Удаляем пустые строки
-        if (ftl_dst.endswith('\n')):
-            ftl_dst = ftl_dst[:-1] # Удаляем перенос строки из конца данных, если есть
+            ftls_raw = res.json()
+            ftls_allitems.extend(ftls_raw['items'])
 
-        ftl_src_lines = ftl_src.count('\n')
-        ftl_dst_lines = ftl_dst.count('\n')
-
-        # Случай 3 -- Число строк в требуемой локали
-        # не соответствует числу строк в источнике
-        if (ftl_src_lines != ftl_dst_lines):
-            if (abs(ftl_src_lines - ftl_dst_lines) >= 20):
-                print(f'⏫ {ftl_dst_meta["html_url"]}')
+            if len(ftls_raw['items']) < 30:
+                break # this means this page was the last
             else:
-                print(f'🔼 {ftl_dst_meta["html_url"]}')
+                ftls_page += 1 # go to next page
+
+        # CASE 01 -- There is nothing to translate
+        if (len(ftls_allitems) == 0):
+            print('\n➖ No ftls found')
             continue
 
 
-        res = make_request(
-            f'https://api.github.com/repos/pop-os/{repo}/commits',
-            headers={
-                'Authorization': f'Bearer {a}'
-            },
-            params={
-                'path': ftl_src_raw['path'], # Чтобы получить только историю этого файла
-                'per_page': 1 # Чтобы получить только последний коммит
-            }
-        )
+        ftls_src_meta = list(filter(lambda m: f'/{LOCALE_SRC}/' in m['path'], ftls_allitems))
+        ftls_dst_meta = list(filter(lambda m: f'/{LOCALE_DST}/' in m['path'], ftls_allitems))
+        names_src = {m['name'] for m in ftls_src_meta}
+        names_dst = {m['name'] for m in ftls_dst_meta}
 
-        ftl_src_date_raw = res.json()
-        ftl_src_date = ftl_src_date_raw[0]['commit']['committer']['date'] # Достаём время коммита
-        ftl_src_date = datetime.strptime(ftl_src_date, '%Y-%m-%dT%H:%M:%SZ') # Парсим время...
-        ftl_src_date = ftl_src_date.timestamp() # ...и получаем таймштамп!
+        for name in names_src:
+            ftl_src_meta = next((m for m in ftls_src_meta if m['name'] == name), None)
+            ftl_src_text = get_ftl_text(ftl_src_meta['url'])
+            ftl_src_strs = ftl_src_text.count('\n') + 1
 
-        res = make_request(
-            f'https://api.github.com/repos/pop-os/{repo}/commits',
-            headers={
-                'Authorization': f'Bearer {a}'
-            },
-            params={
-                'path': ftl_dst_raw['path'], # Чтобы получить только историю этого файла
-                'per_page': 1 # Чтобы получить только последний коммит
-            }
-        )
+            if (name not in names_dst):
+                # CASE 02 -- Target locale is missing completely
+                print(f'\n❌ 0/{ftl_src_strs} (0%) -- {LOCALE_DST}/{name} does not exist in https://github.com/pop-os/{repo}')
+                continue
 
-        ftl_dst_date_raw = res.json()
-        ftl_dst_date = ftl_dst_date_raw[0]['commit']['committer']['date'] # Достаём время коммита
-        ftl_dst_date = datetime.strptime(ftl_dst_date, '%Y-%m-%dT%H:%M:%SZ') # Парсим время...
-        ftl_dst_date = ftl_dst_date.timestamp() # ...и получаем таймштамп!
+            ftl_dst_meta = next((m for m in ftls_dst_meta if m['name'] == name), None)
+            ftl_dst_text = get_ftl_text(ftl_dst_meta['url'])
+            ftl_dst_strs = ftl_dst_text.count('\n') + 1
 
-        # Случай 4 -- Дата посл. коммита источника более
-        # поздняя, чем дата посл. коммита требуемой локали
-        if (ftl_src_date > ftl_dst_date):
-            print(f'⬆️ {ftl_dst_meta["html_url"]}')
-            continue
+            # CASE 03 -- Amount of strings in target ftl file
+            # doesn't match with amount of strings in source ftl file
+            if (ftl_src_strs != ftl_dst_strs):
+                print(
+                    f'{"\n⏫ " if (abs(ftl_src_strs - ftl_dst_strs) >= 20) else "\n🔼 "}'
+                    + f'{ftl_dst_strs}/{ftl_src_strs} '
+                    + f'({int(ftl_dst_strs/ftl_src_strs*100)}%) -- {ftl_dst_meta["html_url"]}'
+                )
+                continue
 
 
-        # Наконец, случай 5 -- Перевод ftl-файла актуален
-        # и содержит совпадающее количество строк (ура!)
-        print(f'✅ {ftl_dst_meta["html_url"]}')
+            ftl_src_date = get_ftl_date(repo, ftl_src_meta['path'])
+            ftl_dst_date = get_ftl_date(repo, ftl_dst_meta['path'])
+
+            # CASE 04 -- No difference in amount of strings, but last
+            # source ftl file is newer than last target ftl file
+            if (ftl_src_date > ftl_dst_date):
+                print(f'\n⬆️ Newer src ftl -- {ftl_dst_meta["html_url"]}')
+                continue
 
 
-print(f'\n=== Итоговое число запросов: {rq_cnt} ({time_cnt} сек.) | FTL_LOOKUP v1.0.1 (24-10-28) ===')
-exit(0)
+            # Finally, CASE 05 -- Target ftl file is both up-to-date
+            # and has the same amount of strings as source ftl file (Ура!)
+            print(f'\n✅ {ftl_dst_strs}/{ftl_src_strs} (100%) -- {ftl_dst_meta["html_url"]}')
+
+
+    print(f'\n=== Total requests: {req_cnt} ({req_sec} sec) | FTL_LOOKUP v1.1.0 (25-09-05) ===')
+    exit(0)
